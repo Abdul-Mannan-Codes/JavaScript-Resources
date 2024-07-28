b3d4838bd5c69c49dcab86873e99ff99ed6ef1f9
Abdul-Mannan-Codes <sdabmannan786@gmail.com>
2024-07-28 21:00:41 +0530

    theory table added

---
+++ script.js
@@ -0,0 +1,4 @@
+const manualPath = "https://msbte.engg-info.website/sites/default/files/CO%2C%20CM%2C%20CW_1/22519%20-%20Client%20Side%20Scripting%20Language-syllabus.pdf";
+function locateToManual(){
+    window.location.href = manualPath;
+}
