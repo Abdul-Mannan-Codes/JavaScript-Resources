eb77a9c4764e97e8898df3964a912308783171cf
Abdul-Mannan-Codes <sdabmannan786@gmail.com>
2024-07-29 17:58:25 +0530

    Practical table created

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const manualPath = "https://msbte.engg-info.website/sites/default/files/CO%2C%20CM%2C%20CW_1/22519%20-%20Client%20Side%20Scripting%20Language-syllabus.pdf";
+const header = document.querySelector('header');
 function locateToManual(){
     window.location.href = manualPath;
 }
+window.addEventListener('scroll', ()=> {
+    if(window.scrollY > 500){
+        header.style.position = 'fixed';
+    }
+    else{
+        header.style.position = '';
+    }
+})
